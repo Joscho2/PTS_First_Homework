d6436003e85be88ddba6ef9c703b73502043e79b
Jozef Martiš <mjjozef@gmail.com>
2017-03-11 22:29:04 +0100

    Spracovanie vstupu, krute posielanie argumentov spolu s testovacimi vypismi

--- main.py
+++ main.py
@@ -11,19 +11,51 @@ def zisti_heslo():
 def vstup_heslo():
        return ulozene_heslo.hexdigest() == hashlib.sha512(str_encode(zisti_heslo())).hexdigest()
 
-def not_found(string):
-    print("Príkaz \"" + string + "\" nenájdený!")
+def error():
+    print("Neplatný vstup!")
 
+def cmd_points(d):
+    name = d[1]
+    number = d[2]
+    print("Points " + name + " " + number)
+
+def cmd_reduce(d):
+    number = d[1]
+    print("Reduce " + number)
+
+def cmd_junior(d):
+    name = d[1]
+    print("Junior " + name)
+
+def cmd_ranking(l):
+    if len(l) >= 1:
+        rank_junior()
+    else:
+        rank_all()
+
+def rank_all():
+    print("All")
+
+def rank_junior():
+    print("Junior")
+def quit(d):
+    exit()
 #################################  
-  
-#while True:
-#   vstup = input.split(" ")
-#    commnad = {
-#        
-#    }.get(vstup[0], not_found(vstup))
+ 
+ulozene_heslo = hashlib.sha512(str_encode(zisti_heslo())); 
 
-ulozene_heslo = hashlib.sha512(str_encode(zisti_heslo()));
+v = []
 
-print("Zadaj este raz heslo na otestovanie.")
-print(vstup_heslo())
-    
+command = {
+    'points' : cmd_points,
+    'reduce' : cmd_reduce,
+    'junior' : cmd_junior,
+    'ranking': cmd_ranking, 
+    'quit' : quit
+}
+while True:
+    v = input().split()  
+    try:
+        command[v[0]](dict((i, v[i]) for i in range(1, len(v))))
+    except KeyError:
+        error()     
